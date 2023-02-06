b9853edb3e53d1e283d2bb8ef5f7cd61aab66f1a
kskannandotwin <kskannan.win@gmail.com>
2023-02-06 19:40:11 +0530

    06.02.2023
    236. variables and scoping review of 'var' has completed

---
+++ code.js
@@ -0,0 +1,26 @@
+// es5: we declared variables using var
+
+// global scoped
+var x = 1;
+
+// variables were global scoped or function scoped
+function add1(y) {
+    // function scoped
+    var x = 2;
+    return y + x;
+}
+// 5
+console.log(add1(3));
+
+function add2(y) {
+    return y + x;
+}
+// 4
+console.log(add2(3));
+
+// they were also hoisted
+// undefined, no error - thanks to hoisting
+console.log(z);
+var z = 1;
+// 1
+console.log(z);
