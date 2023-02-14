6c8519d4bbc315b06a5b79630afcbd772fb30b7c
kskannandotwin <kskannan.win@gmail.com>
2023-02-14 19:02:21 +0530

    14.02.2023
    250. functions - rest parameters has completed

---
+++ 15.functions-rest-parameters/code.js
@@ -0,0 +1,37 @@
+// ES5
+function printAll() {
+  var arr = [];
+  for (var i = 0; i < arguments.length; i++) {
+    arr.push(arguments[i]);
+  }
+  return arr;
+}
+console.log(printAll(1, 2));
+
+// ES6 using rest parameters
+function printAll2(x, ...y) {
+  console.log(x);
+  console.log(y);
+}
+console.log(printAll2(1, 2, 3));
+
+// function pCheck firstname, lastname
+// passenger: firstname, lastname
+// belongings: item1, item2, item3 ...
+
+// ES6
+function pCheck(firstname, lastname, ...items) {
+  const belongings = items.length > 0 ? items: 'none';
+  console.log(`Passenger: ${firstname} ${lastname}.
+Belongings: ${belongings}`);
+}
+pCheck('Mark', "Zuckerberg", 'Bag', 'Suitcase');
+
+// ES5
+function pCheck2(firstname, lastname) {
+  console.log(arguments);
+  var z = Array.prototype.slice.call(arguments, 2);
+  var belongings = z.length > 0 ? z : 'none';
+  console.log('Passenger: ' + firstname + ' ' + lastname + '\n' + 'Belongings: ' + belongings);
+}
+pCheck2('Bill', 'Gates', 'Backpack', 'Suitcase');
