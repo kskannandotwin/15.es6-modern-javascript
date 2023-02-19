81197c4fb5433c7a6d1d22beb6fe0515910795a1
kskannandotwin <kskannan.win@gmail.com>
2023-02-19 14:48:47 +0530

    19.02.2023
    262. array of has completed

---
+++ 25.array-of/code.js
@@ -0,0 +1,11 @@
+// create an array of 1 element
+const x  = Array.of(3);
+console.log(x);
+
+// create an array of 3 element
+const y  = Array.of(3, 4, 9);
+console.log(y);
+
+// please don't mix this with the Array constructor
+const z = new Array(3);
+console.log(z); // array with 3 empty slots
