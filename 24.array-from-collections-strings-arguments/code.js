c07fc6637bd5aa30fceb877a850a665f8d2671e2
kskannandotwin <kskannan.win@gmail.com>
2023-02-19 06:46:01 +0530

    19.02.2023
    261. array.from() - optional map, length property, maps and sets has completed

--- 24.array-from-collections-strings-arguments/code.js
+++ 24.array-from-collections-strings-arguments/code.js
@@ -10,6 +10,7 @@ console.log(Array.prototype.map.call(str, function(x) {
     return x + 'a';
 }));
 const collection = document.getElementsByClassName('blue');
+console.log(collection.length);
 console.log(collection);
 console.log(collection.slice);
 // ES5
@@ -28,3 +29,30 @@ function g (x, y) {
     console.log(Array.from(arguments));        
 }
 g(1, 2);
+
+// use the optional map function 
+let str2 = 'Strawberry';
+console.log(Array.from(str2, x => x + ','));
+console.log(Array.from([1, 2, 3], x => x * 2));
+
+// from any object with a length property
+const obj = {
+    length: 3   
+};
+console.log(Array.from(obj, value => 1));
+console.log(Array.from(obj, (value, index) => index * 2));
+
+// create an array from Sets and Maps (iterable objects)
+let mySet = new Set();
+mySet.add('John');
+mySet.add(1);
+console.log(mySet);
+console.log(mySet.size);
+console.log(Array.from(mySet));
+
+const myMap = new Map([[1, 2], [3, 4]]);
+console.log(myMap);
+console.log(Array.from(myMap));
+
+
+
