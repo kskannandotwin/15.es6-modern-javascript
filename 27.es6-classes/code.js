95dd35a7868f359d7caa83b3eab743b498c8d830
kskannandotwin <kskannan.win@gmail.com>
2023-02-20 06:37:07 +0530

    20.02.2023
    265. es6 classes syntax has completed

--- 27.es6-classes/code.js
+++ 27.es6-classes/code.js
@@ -19,14 +19,33 @@ function smartPhone(make, model, OS) {
   this.make = make;
   this.model = model;
   this.OS = OS;
-//   phone.call(this, make, model)
+  //   phone.call(this, make, model)
 }
-smartPhone.prototype = new phone('Samsung', 'S6');
+smartPhone.prototype = new phone("Samsung", "S6");
 // smartPhone.prototype = Object.create(phone.prototype);
 smartPhone.prototype.constructor = smartPhone;
 
-var mySmartphone = new smartPhone('Apple', 'iphone 7', 'IOS 10');
+var mySmartphone = new smartPhone("Apple", "iphone 7", "IOS 10");
 console.log(mySmartphone);
 console.log(mySmartphone.extendWarranty);
 mySmartphone.extendWarranty(3);
 console.log(mySmartphone);
+
+// ES6
+class phone6 {
+  constructor(make, model, warranty, color) {
+    this.make = make;
+    this.model = model;
+    this.warranty = warranty || 24;
+    this.color = color || "white";
+  }
+  extendWarranty(x) {
+      this.warranty += x;
+  }
+}
+
+const myPhone6 = new phone6('Apple', 'Iphone7', 11, 'Black');
+console.log(myPhone6);
+console.log(typeof phone6); // function
+
+
